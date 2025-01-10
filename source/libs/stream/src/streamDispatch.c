f2d6c12a8d62f7b5c787cabc0c9c0b954904a41e
Haojun Liao <hjliao@taosdata.com>
2025-01-10 11:29:17 +0800

    refactor(stream): track the msgId for each upstream tasks.

--- source/libs/stream/src/streamDispatch.c
+++ source/libs/stream/src/streamDispatch.c
@@ -1853,6 +1853,9 @@ int32_t streamProcessDispatchMsg(SStreamTask* pTask, SStreamDispatchReq* pReq, S
     return TSDB_CODE_STREAM_TASK_NOT_EXIST;
   }
 
+  stDebug("s-task:%s lastMsgId:%"PRId64 " for upstream taskId:0x%x(vgId:%d)", id, pInfo->lastMsgId, pReq->upstreamTaskId,
+          pReq->upstreamNodeId);
+
   if (pMeta->role == NODE_ROLE_FOLLOWER) {
     stError("s-task:%s task on follower received dispatch msgs, dispatch msg rejected", id);
     status = TASK_INPUT_STATUS__REFUSED;
